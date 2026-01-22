7310c0851c72d0db001dc00edad201dc31120e5d
Fernando Martínez <fernando_m1911a1@hotmail.com>
2026-01-22 00:28:38 -0600

    Provides contexts in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { UserProvider } from './contexts/user-context.tsx'
+import { UsersProvider } from './contexts/users-context.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <UsersProvider>
+      <UserProvider>
+        <App />
+      </UserProvider>
+    </UsersProvider>
   </StrictMode>,
 )
