9c6476a7bac871ff3a4b881933756821313c9e39
Fernando Martínez <fernando_m1911a1@hotmail.com>
2026-01-25 01:22:51 -0600

    Refactors form using form actions and form data.

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -1,44 +1,41 @@
 import type React from "react";
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import { UsersContext } from "../contexts/users-context";
 import { UserCard } from "./user-card";
-import "./login.css";
 import { UserContext } from "../contexts/user-context";
+import "./login.css";
 
 export const Login: React.FC<{}> = () => {
-    const [formInput, setFormInput] = useState({
-        email: '',
-        password: ''
-    });
-    const [error, setError] = useState<string>('');
-
+    const [ error, setError ] = useState<string>("");
     const { users } = useContext(UsersContext);
     const { setCurrentUser } = useContext(UserContext);
 
-    useEffect(() => {
-       if (error) {
-            setFormInput({ email: '', password: '' });
-       }
-    }, [error])
+    const validateUser = (formData: FormData) => {
+        const formValues = Object.fromEntries(formData.entries())
+        const { email, password } = formValues as { email: string; password: string };
+        const foundUser = users?.find(user => user.email === email && user.password_hash === password);
 
-
-    const validateUser = () => {
-        const foundUser = users?.find(user => user.email === formInput.email && user.password_hash === formInput.password);
         if (foundUser) {
             setCurrentUser(foundUser);
         } else {
             setError('Invalid email or password. Please try again.');
         }
-    };
+    }
+    
+    const removeErrorMsg = () => {
+        if (error) {
+            setError('');
+        }
+    }
 
     return (
         <>
             <h1>My Todo App</h1>
             <div className="form-container">
-                <form>
-                    <input type="text" placeholder="email" value={formInput.email} onClick={() => setError('')} onChange={(e) => setFormInput({...formInput, email: e.target.value})}/>
-                    <input type="password" placeholder="Password" value={formInput.password} onClick={() => setError('')} onChange={(e) => setFormInput({...formInput, password: e.target.value})} />
-                    <button type="button" onClick={() => validateUser()}>Login</button>
+                <form action={validateUser} onInput={removeErrorMsg}>
+                    <input type="text" placeholder="email" name="email" />
+                    <input type="password" placeholder="Password" name="password" />
+                    <button type="submit">Login</button>
                     <span style={{ color: 'red' }}>{error}</span>
                 </form>
             </div>
