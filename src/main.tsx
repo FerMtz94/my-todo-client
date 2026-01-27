f3b1764359d4fb54434810a8f4f152071829568d
Fernando Martínez <fernando_m1911a1@hotmail.com>
2026-01-27 01:43:34 -0600

    Provides task context to main component.

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import { UserProvider } from './contexts/user-context.tsx'
-import { UsersProvider } from './contexts/users-context.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import { TaskProvider } from "./contexts/tasks-context.tsx";
+import { UserProvider } from "./contexts/user-context.tsx";
+import { UsersProvider } from "./contexts/users-context.tsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <UsersProvider>
-      <UserProvider>
-        <App />
-      </UserProvider>
-    </UsersProvider>
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(
+	<StrictMode>
+		<UsersProvider>
+			<UserProvider>
+				<TaskProvider>
+					<App />
+				</TaskProvider>
+			</UserProvider>
+		</UsersProvider>
+	</StrictMode>,
+);
