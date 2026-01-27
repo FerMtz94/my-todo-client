8fc6cee9d8332492c5751028bf7f181c54f55be9
Fernando Martínez <fernando_m1911a1@hotmail.com>
2026-01-27 01:19:44 -0600

    Fixes formatting in "task" files.

--- src/contexts/tasks-context.tsx
+++ src/contexts/tasks-context.tsx
@@ -1,15 +1,17 @@
-import React, { createContext, type PropsWithChildren } from "react"
-import type { Task } from "../types/task"
-import type { TaskContextType } from "../types/task-context"
+import React, { createContext, type PropsWithChildren } from "react";
+import type { Task } from "../types/task";
+import type { TaskContextType } from "../types/task-context";
 
-export const TaskContext = createContext<TaskContextType>({} as TaskContextType)
+export const TaskContext = createContext<TaskContextType>(
+	{} as TaskContextType,
+);
 
 export const TaskProvider: React.FC<PropsWithChildren> = ({ children }) => {
-  const [tasks, setTasks] = React.useState<Task[] | null>(null)
+	const [tasks, setTasks] = React.useState<Task[] | null>(null);
 
-  const contextValue: TaskContextType = { tasks, setTasks }
+	const contextValue: TaskContextType = { tasks, setTasks };
 
-  return (
-    <TaskContext.Provider value={contextValue}>{children}</TaskContext.Provider>
-  )
-}
+	return (
+		<TaskContext.Provider value={contextValue}>{children}</TaskContext.Provider>
+	);
+};
