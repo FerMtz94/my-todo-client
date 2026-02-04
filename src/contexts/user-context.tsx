5eafe4ec5bbbf2ba7504f92056ddafde96019849
Fernando Martínez <fernando_m1911a1@hotmail.com>
2026-02-03 20:47:49 -0600

    Improves formatting in user contexts.

--- src/contexts/user-context.tsx
+++ src/contexts/user-context.tsx
@@ -1,15 +1,16 @@
-import React, { createContext, type PropsWithChildren } from 'react';
-import type { User } from '../types/user';
-import type { UserContextType } from '../types/user-context';
+import React, { createContext, type PropsWithChildren } from "react";
+import type { User } from "../types/user";
+import type { UserContextType } from "../types/user-context";
 
-export const UserContext = createContext<UserContextType>({} as UserContextType);
+export const UserContext = createContext<UserContextType>(
+	{} as UserContextType,
+);
 
 export const UserProvider: React.FC<PropsWithChildren> = ({ children }) => {
-    const [currentUser, setCurrentUser] = React.useState<User | null>(null);
+	const [currentUser, setCurrentUser] = React.useState<User | null>(null);
+	const contextValue: UserContextType = { currentUser, setCurrentUser };
 
-    const contextValue: UserContextType = { currentUser, setCurrentUser };
-
-    return <UserContext.Provider value={contextValue}>
-        {children}
-    </UserContext.Provider>;
-}
+	return (
+		<UserContext.Provider value={contextValue}>{children}</UserContext.Provider>
+	);
+};
--- src/contexts/users-context.tsx
+++ src/contexts/users-context.tsx
@@ -1,15 +1,19 @@
-import React, { createContext, type PropsWithChildren } from 'react';
-import type { User } from '../types/user';
-import type { UsersContextType } from '../types/users-context';
+import React, { createContext, type PropsWithChildren } from "react";
+import type { User } from "../types/user";
+import type { UsersContextType } from "../types/users-context";
 
-export const UsersContext = createContext<UsersContextType>({} as UsersContextType);
+export const UsersContext = createContext<UsersContextType>(
+	{} as UsersContextType,
+);
 
 export const UsersProvider: React.FC<PropsWithChildren> = ({ children }) => {
-    const [users, setUsers] = React.useState<User[] | null>(null);
+	const [users, setUsers] = React.useState<User[] | null>(null);
 
-    const contextValue: UsersContextType = { users, setUsers };
+	const contextValue: UsersContextType = { users, setUsers };
 
-    return <UsersContext.Provider value={contextValue}>
-        {children}
-    </UsersContext.Provider>;
-}
+	return (
+		<UsersContext.Provider value={contextValue}>
+			{children}
+		</UsersContext.Provider>
+	);
+};
