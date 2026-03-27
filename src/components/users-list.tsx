2e7e772017b5e578240eae411a80a7704466710f
Fernando Martínez <fernando_m1911a1@hotmail.com>
2026-03-26 21:50:43 -0600

    Adds spinner while loading users.

--- src/components/users-list.tsx
+++ src/components/users-list.tsx
@@ -1,16 +1,41 @@
-import { useContext } from "react";
-import { UsersContext } from "@/contexts/users-context";
 import "./users-list.css";
+import { Spinner } from "@chakra-ui/react";
+import { useDataFetch } from "@/hooks/use-data-fetch";
+import type { User } from "@/types/user";
 import { UserCard } from "./user-card";
 
 export const UsersList = () => {
-	const { users } = useContext(UsersContext);
+	const {
+		data: users,
+		loading,
+		error,
+	} = useDataFetch("https://my-todo-app-neon-five.vercel.app/users");
+
+	if (loading) {
+		return (
+			<div
+				style={{
+					display: "flex",
+					justifyContent: "center",
+					alignItems: "center",
+					height: "100%",
+				}}
+			>
+				<Spinner />
+			</div>
+		);
+	}
+
+	if (error) {
+		return <p>Error loading users: {error}</p>;
+	}
 
 	return (
 		<section id="users-list">
 			<h4>Users list (for dev purposes)</h4>
+			{!users || users.length === 0}
 			<div className="user-cards-container">
-				{users?.map((user) => (
+				{users?.map((user: User) => (
 					<UserCard key={user.id} user={user} />
 				))}
 			</div>
